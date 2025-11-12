b1e00da0b448ba29c737f680abab3582f8f83f12
TheBestCupcake <cavaligrooby@gmail.com>
2025-11-12 13:16:55 +1300

    Navbar functionality added to navigate between the pages.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,34 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Anime from "../pages/Anime";
+import Games from "../pages/Games";
+import Home from "../pages/Home";
+
+let routeVariables = [
+  { path: "/", name: <Home />, component: "Home" },
+  { path: "/Anime", name: <Anime />, component: "Anime" },
+  { path: "/Games", name: <Games />, component: "Games" },
+];
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const browserRouterObjects = routeVariables.map((variable) => (
+    <Link to={variable.path} className="d-inline-flex">
+      {variable.name}
+    </Link>
+  ));
+
+  const routesObjects = routeVariables.map((variable) => (
+    <Route path={variable.path} element={variable.component} />
+  ));
+
+  return (
+    <>
+      <BrowserRouter>
+        <nav>{browserRouterObjects}</nav>
+
+        <Routes>{routesObjects}</Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default Navbar;
