6f5d1d7b331856ba33625f5c89088513c0549c9d
TheBestCupcake <cavaligrooby@gmail.com>
2025-11-12 13:16:27 +1300

    Base pages created for testing purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
     <div>
-      {alertVisible && (
-        <Alert onClick={() => setAlertVisibility(false)}>Bogos Binted</Alert>
-      )}
-      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
-        Button Goes Binted.
-      </Button>
+      <Navbar></Navbar>
     </div>
   );
 }
