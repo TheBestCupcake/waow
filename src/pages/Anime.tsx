3866f6454bcc394eb43d68e4494aec2a9819c476
TheBestCupcake <cavaligrooby@gmail.com>
2025-11-12 15:22:07 +1300

    Page now displays a hard coded video. Framework will be used later for dynamic video streaming.

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,5 +1,15 @@
 function Anime() {
-  return <div>Anime</div>;
+  return (
+    <>
+      <h1>Gigi Murin???</h1>
+      <div className="videoContainer">
+        <video controls className="videoDisplay">
+          <source src="src/database/gigi.mp4" type="video/mp4"></source>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </>
+  );
 }
 
 export default Anime;
