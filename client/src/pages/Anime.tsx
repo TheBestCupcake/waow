e574bf6fbd6942bd9fcc41a76e760caa6259c465
TheBestCupcake <cavaligrooby@gmail.com>
2025-11-14 13:45:04 +1300

    Added useEffect to Anime section to load a video from the backend.

--- client/src/pages/Anime.tsx
+++ client/src/pages/Anime.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+import { fetchVideos } from "../utils/videoServices";
+
 function Anime() {
+  //Logic for getting the video url.
+  const [videoUrl, setVideoUrl] = useState("");
+  const [videoName, setVideoName] = useState("");
+
+  useEffect(() => {
+    const loadVideo = async () => {
+      const data = await fetchVideos();
+      setVideoUrl(data.url);
+      setVideoName(data.name);
+    };
+
+    loadVideo();
+  }, []);
+
   return (
     <>
-      <h1>Gigi Murin???</h1>
+      <h1>{videoName}</h1>
       <div className="videoContainer">
-        <video controls className="videoDisplay">
-          <source src="src/database/gigi.mp4" type="video/mp4"></source>
-          Your browser does not support the video tag.
-        </video>
+        {videoUrl ? ( //CHecks for videoUrl to not be empty first.
+          <video controls className="videoDisplay">
+            <source src={videoUrl} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <p>Loading video...</p> //This displays while videoUrl is loading.
+        )}
       </div>
     </>
   );
