c1615362cd5ba5bee83eccae7265bd234b658d01
TheBestCupcake <cavaligrooby@gmail.com>
2026-05-02 14:24:00 +1200

    Created races.tsx and added human. Fleshed out testing race page.

--- client/src/pages/barony/Barony_Race.tsx
+++ client/src/pages/barony/Barony_Race.tsx
@@ -1,5 +1,30 @@
+import { useParams } from "react-router-dom";
+
+import { races } from "../../data/races";
+
 function Barony_Race() {
-  return <div></div>;
+  const { class: raceName } = useParams();
+
+  const raceInfo = races.find(
+    (ra) => ra.id.toLowerCase() == raceName?.toLowerCase(),
+  );
+
+  if (!raceInfo) {
+    return <h1>Class not found</h1>;
+  }
+
+  return (
+    <div className="baronyContainer">
+      <div className="barony">
+        <h1>{raceInfo.id}</h1>
+      </div>
+
+      <div className="baronyDescription">
+        <h1>Description</h1>
+        <p>{raceInfo.description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Barony_Race;
